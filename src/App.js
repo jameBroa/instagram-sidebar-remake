7887e4559d66d36985cfce5f597606293d8c6268
jameBroa <jbroadhurst2802@gmail.com>
2023-09-18 16:14:42 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1 class="text-3xl font-bold underline">This is a test</h1>
+      <ul>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
